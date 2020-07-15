a85b38440cb2cdde1c3dab8cf25c4249cb552750
Reyoth <serialkiwi@gmail.com>
2020-07-15 14:23:17 +0200

    ajout commande de base

--- spytank/basic_remote/remoteClavier.py
+++ spytank/basic_remote/remoteClavier.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+
+import spytank
+print("taper z pour avancer")
+print("taper s pour reculer")
+lettre = input(">> ")
+
+if lettre == "z":
+    spytank.avance(100)
+elif lettre == "s":
+    spytank.recule(100)
