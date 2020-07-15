78a75d219baa1e7bcec61c5e9ddebba4057bef0b
Reyoth <serialkiwi@gmail.com>
2020-07-15 15:28:21 +0200

    ajout event

--- spytank/basic_remote/remoteEvent.py
+++ spytank/basic_remote/remoteEvent.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python
+import click
+import spytank
+
+z= "z: avancer"
+s = "s: reculer"
+q = "q: tourner a gauche"
+d = "d: tourner a droite"
+x = "x: stop"
+a = "a: augmenter la vitesse"
+e = "e: diminuer la vitesse"
+
+print(z,s,q,d,x,a,e)
+continuer = True
+vitesse = 100
+
+while continuer:
+    lettre = click.getchar(">>")
+    if lettre == "z":
+        spytank.avance(vitesse)
+    elif lettre == "s":
+        spytank.recule(vitesse)
+    elif lettre == "q":
+        spytank.gauche(vitesse)
+    elif lettre == "d":
+        spytank.droite(vitesse)
+    elif lettre == "x":
+        spytank.stop()
+    elif lettre =="a":
+        if vitesse <100:
+            vitesse = vitesse+10
+    elif lettre =="e":
+        if vitesse > 0:
+            vitesse = vitesse-10
+    elif lettre == "p":
+        continuer = False
+        spytank.stop()
